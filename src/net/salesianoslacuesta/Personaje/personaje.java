2fdb627da5b0a66d8b1529987a43083279214bfa
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-31 13:52:32 +0100

    Aqui añadimos los getters y setters necesarios de cada atributo

--- src/net/salesianoslacuesta/Personaje/personaje.java
+++ src/net/salesianoslacuesta/Personaje/personaje.java
@@ -11,5 +11,30 @@ public class personaje {
         this.nivel = nivel;
         this.energia = 100;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void SetNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public int setNivel() {
+        return energia;
+    }
+
+    public int getNivel(){
+        return nivel;
+    }
+
+    public int getEnergia() {
+        return energia;
+    }
+
+    public void setEnergia(int energia) {
+        this.energia = energia;
+    }
     
 }
