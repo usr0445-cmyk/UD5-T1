92e001b2abb640c555a6018820a10c73b71509de
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-31 14:09:15 +0100

    Aqui se añade el atributo necesario,  y el constructor mago necesario

--- src/net/salesianoslacuesta/Mago/mago.java
+++ src/net/salesianoslacuesta/Mago/mago.java
@@ -3,4 +3,10 @@ import net.salesianoslacuesta.Personaje.personaje;
 
 public class mago extends personaje  {
     
+    private String elemento;
+
+    public mago(String nombre, int nivel, String elemento){
+        super(nombre, nivel);
+        this.elemento = elemento;
+    }
 }
