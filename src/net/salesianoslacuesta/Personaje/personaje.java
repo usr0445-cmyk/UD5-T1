54d8f1294470251c5768a4ff4592a4709704c94a
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-31 13:51:21 +0100

    Ponemos los atributos de la clase personaje, y aqui el constructor de la clase personaje con sus parametros

--- src/net/salesianoslacuesta/Personaje/personaje.java
+++ src/net/salesianoslacuesta/Personaje/personaje.java
@@ -2,4 +2,14 @@ package net.salesianoslacuesta.Personaje;
 
 public class personaje {
     
+    private String nombre;
+    private int nivel;
+    private int energia;
+
+    public personaje(String nombre, int nivel, int energia){
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.energia = 100;
+    }
+    
 }
