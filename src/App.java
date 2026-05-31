e5198a3cdd04544e9fed13478ac4c00eba1b7723
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-31 14:17:50 +0100

    Primero hacemos este personaje para probar el funcionamiento de la clase personaje

--- src/App.java
+++ src/App.java
@@ -4,6 +4,10 @@ import net.salesianoslacuesta.Personaje.personaje;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
+        personaje viajero = new personaje("Eldrin", 3, 30);
+        viajero.entrenar(4);
+        viajero.gastarEnergia(30);
+        viajero.mostrarInfo();
     }
 }
