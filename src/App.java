73fef3f7c05017569c7cbf66d11e03a5431b7aca
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-31 14:15:07 +0100

    Preparamos la clase ejecutable con  los imports de las 3 clases anteriores, y añadiendo algo necesario para la App

--- src/App.java
+++ src/App.java
@@ -1,3 +1,9 @@
+import net.salesianoslacuesta.Guerrero.guerrero;
+import net.salesianoslacuesta.Mago.mago;
+import net.salesianoslacuesta.Personaje.personaje;
+
 public class App {
-    
+    public static void main(String[] args) throws Exception {
+        
+    }
 }
