a3e13706bfc974e6f4deb978d64fda60c0a8e8fa
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-31 14:01:29 +0100

    Creamos atribtuto String para el guerrero, y hacemos el constructor gurrero que hereday y tambien añadimos super para llamar al constructor de personaje

--- src/net/salesianoslacuesta/Guerrero/guerrero.java
+++ src/net/salesianoslacuesta/Guerrero/guerrero.java
@@ -3,5 +3,11 @@ import net.salesianoslacuesta.Personaje.personaje;
 
 public class guerrero extends personaje {
     
+    private String arma;
+
+    public guerrero(String nombre, int nivel, String arma){
+        super(nombre, nivel);
+        this.arma = arma;
+    }
 
 }
